90ee92e53a3809135992408d3fa54e06673e975b
i-moonlight <infinite.moonlight0506@gmail.com>
2020-03-08 23:22:07 +0330

    first commit

---
+++ UI/src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ImageDto } from './models';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AppComponent implements OnInit {
+  Images: ImageDto[];
+  keyword = '';
+  isLoading = false;
+  readonly url = 'http://localhost:5000/v1/AirTeam/Search';
+
+  constructor(private changeRef: ChangeDetectorRef, private httpclient: HttpClient) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  Find(value: string): void {
+      this.keyword = value;
+      const isValid = this.keyword && this.keyword.trim().length > 2;
+      if (!isValid) {
+        return;
+      }
+
+      this.isLoading = true;
+      this.httpclient.get<ImageDto[]>(this.url + '?keyword=' + value).subscribe(data => {
+          this.Images = data;
+          this.isLoading = false;
+          this.changeRef.detectChanges();
+      }, error => {
+          this.Images = [];
+          this.isLoading = false;
+          this.changeRef.detectChanges();
+      });
+  }
+
+  onKey(event: any) {
+    if (event.keyCode === 13) {
+      this.Find(event.target.value);
+    }
+  }
+}
---
+++ UI/src/app/models.d.ts
@@ -0,0 +1,7 @@
+export interface ImageDto {
+    imageId: string;
+    title: string;
+    description: string;
+    baseImageUrl: string;
+    detailUrl: string;
+}
