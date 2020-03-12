d6d36bade1810ea3ab69110dd859e209dc46e597
i-moonlight <infinite.moonlight0506@gmail.com>
2020-03-12 20:19:24 +0330

    update app test

--- UI/src/app/app.component.spec.ts
+++ UI/src/app/app.component.spec.ts
@@ -58,7 +58,7 @@ describe('AppComponent', () => {
     const button: HTMLButtonElement = compiled.querySelector('button');
     button.click();
 
-    const req = httpMock.expectOne(`${app.URL}?keyword=${keyword}`);
+    const req = httpMock.expectOne(`${app.BaseURL}?keyword=${keyword}`);
     req.flush(dummyImages);
 
     expect(req.request.method).toBe('GET');
