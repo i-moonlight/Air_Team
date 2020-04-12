663c33c0668a05c93cc50ff56c176283dd104902
i-moonlight <infinite.moonlight0506@gmail.com>
2020-04-12 20:10:33 +0430

    e2e test fix

--- UI/e2e/src/app.e2e-spec.ts
+++ UI/e2e/src/app.e2e-spec.ts
@@ -1,5 +1,5 @@
-import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
+import { AppPage } from './app.po';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('AirTeamUI app is running!');
+    expect(page.getButtonText()).toEqual('Search');
   });
 
   afterEach(async () => {
--- UI/e2e/src/app.po.ts
+++ UI/e2e/src/app.po.ts
@@ -5,7 +5,7 @@ export class AppPage {
     return browser.get(browser.baseUrl) as Promise<unknown>;
   }
 
-  getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+  getButtonText(): Promise<string> {
+    return element(by.css('app-root .search-row button')).getText() as Promise<string>;
   }
 }
