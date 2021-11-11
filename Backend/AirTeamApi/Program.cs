0c20761505db64d32ecdf12f62769b09d0c0f4e4
i-moonlight <infinite.moonlight0506@gmail.com>
2021-11-11 16:23:47 +0330

    move global using to program.cs

--- Backend/AirTeamApi/Program.cs
+++ Backend/AirTeamApi/Program.cs
@@ -1,3 +1,9 @@
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Threading;
+global using System.Threading.Tasks;
+
 using AirTeamApi;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
--- Backend/AirTeamApi/Startup.cs
+++ Backend/AirTeamApi/Startup.cs
@@ -1,9 +1,3 @@
-global using System;
-global using System.Collections.Generic;
-global using System.Linq;
-global using System.Threading;
-global using System.Threading.Tasks;
-
 using AirTeamApi.HealthCheck;
 using AirTeamApi.Services.Contract;
 using AirTeamApi.Services.Impl;
